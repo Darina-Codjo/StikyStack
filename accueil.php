cf519fc35cfb6110f8c6be3a11e2683dc2298f66
Darina <darina.codjo@universite-paris-saclay.fr>
2024-04-30 13:59:18 +0200

    Clean code

--- accueil.php
+++ accueil.php
@@ -1,63 +1,21 @@
 
 <div class="container-fluide">
-    <div class="row">
+
+    <!-- ELEMENT DE GAUCHE -->
 
 
-        <!-- ELEMENT DE GAUCHE -->
-        <aside class="col-md-2 border" style="background-color:white; margin-left: 60px; border-radius: 30px;">
-            <br>
-            <div class="left-link">
-                <a class="nav-link nav-left" href="index.php?module=lire_article&action=afficher_liste_article&idSection=1">La tête</a>
-            </div>
-            <div class="left-link">
-                <a class="nav-link nav-left" href="index.php?module=lire_article&action=afficher_liste_article&idSection=2">Les bras</a>
-            </div>
-            <div class="left-link">
-                <a class="nav-link nav-left" href="index.php?module=lire_article&action=afficher_liste_article&idSection=3">Le torse</a>
-            </div>
-            <div class="left-link">
-                <a class="nav-link nav-left" href="index.php?module=lire_article&action=afficher_liste_article&idSection=4">La reproduction</a>
-            </div>
-            <div class="left-link">
-                <a class="nav-link nav-left" href="index.php?module=lire_article&action=afficher_liste_article&idSection=5">Les jambes</a>
-            </div>
-        </aside>
+    <!-- ELEMENT DU MILIEU -->
+        <br>
+        <br>
+        <h1>BIENVENUE !</h1>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
 
-        <!-- ELEMENT DU MILIEU -->
-        <section class="col-md-7 border text-center" style="background-color:white; border-radius: 30px;">
-        	<br>
-        	<br>
-            <h1>BIENVENUE !</h1>
-            <br>
-            <img src="../img/Bitmoji/bienvenue.png" class="img-center">
-            <br>
-            <br>
-            <br>
-
-            <br>
-            <p></p>
-        </section>
-
-        <!-- ELEMENT DE DROITE -->
-        <aside class="col-md-2 border droite" style="width: 12rem; border-radius: 30px; text-align: center; background-color: white;">
-			<div style="text-align: center;">
-				<br>
-				<h4 style="text-align: center;">Faites un quiz !</h4>
-    			<br>
-				<div class="card" style="background-color: #161727; border-radius: 30px;">
-				<br>
-				</div>
-				<br>
-				<div class="card" style="background-color: #161727; border-radius: 30px;">
-				<br>
-				</div>
-				<br>
-				<div class="card" style="background-color: #161727; border-radius: 30px;">
-				<br>
-				</div>
-			</div>
-		</aside>
-        <!-- FIN -->
-    </div>
+    <!-- ELEMENT DE DROITE -->
+    
+    <!-- FIN -->
 </div>
 <br>
