805e1a2b6c7bfecf947d244d88c1b22a191cf4a3
Darina <darina.codjo@universite-paris-saclay.fr>
2024-05-04 19:35:17 +0200

    db name

--- connexion.php
+++ connexion.php
@@ -5,7 +5,7 @@
 
 		public static function initConnexion(){
 			try {
-				$dns = "mysql:host=localhost;dbname=ter_db;charset=utf8";
+				$dns = "mysql:host=localhost;dbname=stikystack;charset=utf8";
 				$user = "root";
 				$password="";
 				self::$bdd = new PDO($dns,$user,$password);
