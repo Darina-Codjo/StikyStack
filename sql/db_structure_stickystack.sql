e1cb3269cfb1d801f6e6fd23c31c55875ba0af42
NicolasDcz <115233642+NicolasDcz@users.noreply.github.com>
2024-05-06 15:52:43 +0200

    Update db_structure_stickystack.sql
    maj compatibilité phpmyadmin

--- sql/db_structure_stickystack.sql
+++ sql/db_structure_stickystack.sql
@@ -1,26 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le : sam. 04 mai 2024 à 17:30
--- Version du serveur : 8.2.0
--- Version de PHP : 8.2.13
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
---
--- Base de données : `stickystack`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `note`
---
-
 DROP TABLE IF EXISTS `note`;
 CREATE TABLE IF NOT EXISTS `note` (
   `idNote` int NOT NULL AUTO_INCREMENT,
@@ -29,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `note` (
   `creationDate` datetime(6) NOT NULL,
   `updateDate` datetime(6) NOT NULL,
   PRIMARY KEY (`idNote`)
-) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- --------------------------------------------------------
 
@@ -46,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `updateDate` datetime(6) NOT NULL,
   `password` varchar(100) NOT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- --------------------------------------------------------
 
@@ -54,17 +31,12 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Structure de la table 'link'
 --
 
-DROP TABLE IF EXISTS 'link'
-CREATE TABLE IF NOT EXISTS 'link' (
-'idLink int,
-'idUser' int,
-'idNote'int,
-FOREIGN KEY (idUser) REFERENCES user(idUser),
-FOREIGN KEY (idNote) REFERENCES note(idNote),
-PRIMARY KEY (idLink),
-) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+DROP TABLE IF EXISTS `link`;
+CREATE TABLE IF NOT EXISTS `link` (
+`idLink` int,
+`idUser` int,
+`idNote`int,
+FOREIGN KEY (`idUser`) REFERENCES user(`idUser`),
+FOREIGN KEY (`idNote`) REFERENCES note(`idNote`),
+PRIMARY KEY (`idLink`)
+) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
