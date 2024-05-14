6a4d2268667a04424e5b5258c7d7972e0a4ff5d7
ATAGUEMA Feriel <feriel.ataguema@universite-paris-saclay.fr>
2024-05-14 15:25:45 +0200

    edit acuueil

--- comp_post_its/vue_compPost.php
+++ comp_post_its/vue_compPost.php
@@ -122,30 +122,30 @@ $fnameUser = $_SESSION['first_name'];
 </head>
 <body>
 <header>
-    <h1>Bienvenue, <?php echo htmlspecialchars($fnameUser); ?>!</h1>
+    <h1>Welcome, <?php echo htmlspecialchars($fnameUser); ?>!</h1>
     <nav>
-        <a href="deconnexion.php">Déconnexion</a>
+        <a href="deconnexion.php">Sign Out</a>
     </nav>
 </header>
 <main>
     <div class="post-it-section">
-        <h2>Mes Post-its</h2>
-        <button class="add-button" onclick="openForm()">Ajouter Post-its</button>
+        <h2>My Post-its</h2>
+        <button class="add-button" onclick="openForm()">Add Post-its</button>
         <div class="post-it-display" id="mesPostIts"></div>
     </div>
     <div class="post-it-section" style="background-color: #ffcab0;">
-        <h2>Post-its partagés</h2>
-        <div class="post-it-display" id="postItsPartages"></div>
+        <h2>Post-its Shared</h2>
+        <div class="post-it-display" id="postItsShared"></div>
     </div>
 </main>
 
 <div class="form-popup" id="myForm">
     <form class="form-container" action="submit_postit.php" method="post">
         <h1>Ajouter Post-it</h1>
-        <label for="titre"><b>Titre</b></label>
-        <input type="text" id="titre" name="titre" placeholder="Entrez le titre" required maxlength="150">
-        <label for="contenu"><b>Contenu</b></label>
-        <textarea placeholder="Écrivez le contenu" name="contenu" required></textarea>
+        <label for="title"><b>title</b></label>
+        <input type="text" id="title" name="title" placeholder="Enter title" required maxlength="150">
+        <label for="content"><b>content</b></label>
+        <textarea placeholder="Écrivez le content" name="content" required></textarea>
         <button type="submit" class="btn">Ajouter</button>
         <button type="button" class="btn cancel" onclick="closeForm()">Fermer</button>
     </form>
@@ -277,11 +277,11 @@ $fnameUser = $_SESSION['first_name'];
     <form class="form-container" onsubmit="modifierPostIt(event)">
         <h1>Modifier Post-Its</h1>
 
-        <label for="edit-titre"><b>Titre</b></label>
-        <input type="text" id="edit-titre" name="edit-titre" required maxlength="50">
+        <label for="edit-title"><b>title</b></label>
+        <input type="text" id="edit-title" name="edit-title" required maxlength="50">
 
-        <label for="edit-contenu"><b>Contenu</b></label>
-        <textarea id="edit-contenu" name="edit-contenu" required
+        <label for="edit-content"><b>content</b></label>
+        <textarea id="edit-content" name="edit-content" required
         maxlength="150"></textarea>
 
         <div>Date d'ajout: <span id="edit-date_ajout"></span></div>
@@ -408,11 +408,11 @@ $fnameUser = $_SESSION['first_name'];
     <form class="form-container" onsubmit="modifierPostIt(event)">
         <h1>Modifier Post-Its</h1>
 
-        <label for="edit-titre"><b>Titre</b></label>
-        <input type="text" id="edit-titre" name="edit-titre" required maxlength="50">
+        <label for="edit-title"><b>title</b></label>
+        <input type="text" id="edit-title" name="edit-title" required maxlength="50">
 
-        <label for="edit-contenu"><b>Contenu</b></label>
-        <textarea id="edit-contenu" name="edit-contenu" required
+        <label for="edit-content"><b>content</b></label>
+        <textarea id="edit-content" name="edit-content" required
         maxlength="150"></textarea>
 
         <div>Date d'ajout: <span id="edit-date_ajout"></span></div>
